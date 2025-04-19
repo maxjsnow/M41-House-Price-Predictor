2f742738db3904f2f3c8759a45362adaedf93e3f
maxjsnow <maxhannon11@gmail.com>
2025-04-19 18:45:44 +0100

    patch - photo removed

--- MLMHP1/MLMHP.py
+++ MLMHP1/MLMHP.py
@@ -4,10 +4,6 @@ import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 
-# --- Keele University Logo (Centered) ---
-col1, col2, col3 = st.columns([1, 2, 1])
-with col2:
-    st.image("images/keele_logo.jpg", use_column_width=True)
 
 # --- Load trained model and objects ---
 with open("rf_model.pkl", "rb") as f:
